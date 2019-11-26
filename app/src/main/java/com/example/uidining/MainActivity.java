8fe01fa4fb42f6bb161c2be79bd07f7ea326c8c8
adutta-eng <adutta7@illinois.edu>
2019-11-26 16:30:37 -0600

    Initial commit

---
+++ app/src/main/java/com/example/uidining/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.uidining;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView textView = findViewById(R.id.text);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://uiuc-api2.herokuapp.com/dining/information";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            textView.setText("this worked" + response.get("Data"));
+                        } catch(JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
+            }
+        });
+        queue.add(jsonObjectRequest);
+    }
+}
