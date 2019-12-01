82b045a3d880fca293b427b64844687b93c3f810
Suchetan Dontha <suchdon777@gmail.com>
2019-12-01 00:27:01 -0500

    Added back button on select dining hall page

--- app/src/main/java/com/example/uidining/SelectDiningHall.java
+++ app/src/main/java/com/example/uidining/SelectDiningHall.java
@@ -19,6 +19,7 @@ public class SelectDiningHall extends AppCompatActivity {
         setContentView(R.layout.activity_select_dining_hall);
         Calendar rightNow = Calendar.getInstance(TimeZone.getTimeZone("CST"));
         String formattedDate = rightNow.get(Calendar.YEAR) + "-" + (rightNow.get(Calendar.MONTH) + 1) + "-" + rightNow.get(Calendar.DAY_OF_MONTH);
+
         Button ikenberryHall = findViewById(R.id.ikenberryHall);
         ikenberryHall.setOnClickListener(unused -> {
 //            System.out.println(rightNow.get(Calendar.YEAR) + "-" + (rightNow.get(Calendar.MONTH) + 1) + "-" + rightNow.get(Calendar.DAY_OF_MONTH));
@@ -27,6 +28,21 @@ public class SelectDiningHall extends AppCompatActivity {
 //            intent.putExtra("Date", formattedDate);
             intent.putExtra("Date","2019-12-2");
             startActivity(intent);
+        });
+
+        Button par = findViewById(R.id.par);
+        par.setOnClickListener(unused -> {
+//            System.out.println(rightNow.get(Calendar.YEAR) + "-" + (rightNow.get(Calendar.MONTH) + 1) + "-" + rightNow.get(Calendar.DAY_OF_MONTH));
+            Intent intent = new Intent(this, VegetarianMeals.class);
+            intent.putExtra("HallID", Constants.PAR);
+            intent.putExtra("Date", formattedDate);
+//            intent.putExtra("Date","2019-12-2");
+            startActivity(intent);
+        });
+
+        Button backtoPrefs = findViewById(R.id.backToPrefs);
+        backtoPrefs.setOnClickListener(unused -> {
+            startActivity(new Intent(this, InputPreferences.class));
         });
     }
 }
