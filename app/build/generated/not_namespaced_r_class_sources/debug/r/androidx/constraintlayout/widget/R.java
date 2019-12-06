b9e8cae82a02b5040e4045b6783f6591359c5150
Suchetan Dontha <suchdon777@gmail.com>
2019-12-05 20:52:02 -0600

    fix switching halls bug

--- app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
@@ -78,15 +78,15 @@ public final class R {
         public static final int gone = 0x7f080075;
         public static final int invisible = 0x7f08008c;
         public static final int left = 0x7f080092;
-        public static final int packed = 0x7f0800b1;
-        public static final int parent = 0x7f0800b5;
-        public static final int percent = 0x7f0800b7;
-        public static final int right = 0x7f0800bc;
-        public static final int spread = 0x7f0800d4;
-        public static final int spread_inside = 0x7f0800d5;
-        public static final int start = 0x7f0800da;
-        public static final int top = 0x7f0800ef;
-        public static final int wrap = 0x7f0800f7;
+        public static final int packed = 0x7f0800b0;
+        public static final int parent = 0x7f0800b4;
+        public static final int percent = 0x7f0800b6;
+        public static final int right = 0x7f0800bb;
+        public static final int spread = 0x7f0800d3;
+        public static final int spread_inside = 0x7f0800d4;
+        public static final int start = 0x7f0800d9;
+        public static final int top = 0x7f0800ee;
+        public static final int wrap = 0x7f0800f6;
     }
     public static final class styleable {
         private styleable() {}
